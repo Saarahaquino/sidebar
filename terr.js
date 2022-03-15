0bc46f43ac0b17a0b2f4405ec3d53d101dcac961
sarahaquino <91294932+Saarahaquino@users.noreply.github.com>
2022-03-15 14:12:31 -0300

    Add files via upload

---
+++ terr.js
@@ -0,0 +1,10 @@
+let sidebar = document.getElementById("sidebar")
+function abrir() {
+    if (sidebar.className == "aberto") {
+        sidebar.className = "fechado"
+    } else {
+        sidebar.className = "aberto"
+    }
+}
+
+
